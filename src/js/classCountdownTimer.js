36335abc29cdabb1da80fb711691f410d9ec7844
Alina Kornienko <a_kornienko@ukr.net>
2021-10-05 17:58:13 +0300

    updated

---
+++ src/js/classCountdownTimer.js
@@ -0,0 +1,45 @@
+class CountdownTimer {
+  constructor({ targetDate, selector }) {
+    this.refs = {
+      days: document.querySelector(`${selector} [data-value="days"]`),
+      hours: document.querySelector(`${selector} [data-value="hours"]`),
+      mins: document.querySelector(`${selector} [data-value="mins"]`),
+      secs: document.querySelector(`${selector} [data-value="secs"]`),
+    };
+
+    this.targetDate = targetDate;
+    this.start();
+  }
+
+  start() {
+    setInterval(() => {
+      const currentDate = Date.now();
+      const deltaTime = this.targetDate - currentDate;
+      this.updateTimer(this.getTimeComponents(deltaTime));
+    }, 1000);
+  }
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+
+  updateTimer({ days, hours, mins, secs }) {
+    this.refs.days.textContent = days;
+    this.refs.hours.textContent = hours;
+    this.refs.mins.textContent = mins;
+    this.refs.secs.textContent = secs;
+  }
+}
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jan 01, 2022'),
+});
